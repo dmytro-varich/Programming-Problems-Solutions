72d806ce8255ef767882875d67a49cb80efdf6c3
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-01-20 22:44:44 +0100

    Add files via upload

---
+++ codewars/7-kyu/PatternCraft - Adapter/solution1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class MarioAdapter : IUnit
+{
+
+    public int attack;
+
+    public MarioAdapter(Mario mario)
+    {
+        this.attack = mario.jumpAttack();
+    }
+
+    public void Attack(Target target)
+    {
+        target.Health -= attack;
+    }
+}
