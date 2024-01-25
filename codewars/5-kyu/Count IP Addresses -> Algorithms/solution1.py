b7d46c8886bc22eea21031f5fe8d59f1ab19a52b
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-01-25 21:27:16 +0100

    Delete codewars/5-kyu/Count IP Addresses -> Algorithms directory

--- codewars/5-kyu/Count IP Addresses -> Algorithms/solution1.py
+++
@@ -1,5 +0,0 @@
-def ips_between(start, end):
-    start = [sum(int (x) * 256**idx for idx, x in enumerate(reversed(start.split("."))))]
-    end = [sum(int (x) * 256**idx for idx, x in enumerate(reversed(end.split("."))))]
-       
-    return end[0] - start[0]
