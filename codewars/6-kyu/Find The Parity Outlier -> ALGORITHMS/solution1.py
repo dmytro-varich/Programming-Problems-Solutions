a6fa098426f25b3f0e0df0d6cc5dc8ba356d0d4d
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-01-25 21:42:26 +0100

    Delete codewars/6-kyu/Find The Parity Outlier -> ALGORITHMS directory

--- codewars/6-kyu/Find The Parity Outlier -> ALGORITHMS/solution1.py
+++
@@ -1,9 +0,0 @@
-def find_outlier(integers):
-    count = sum(1 for num in integers if num % 2 == 0) 
-    
-    if len(integers) - count == 1:
-        result = [integer for integer in integers if integer % 2 != 0]
-    else:
-        result = [integer for integer in integers if integer % 2 == 0]
-    
-    return result[0]
