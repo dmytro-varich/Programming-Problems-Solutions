65bf6414c893cf0cfffb7d12a4e16e6151fd2def
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-06-01 01:00:47 +0200

    Update task_3.sql

--- university-assignments/hrtk/mySQL/task_3.sql
+++ university-assignments/hrtk/mySQL/task_3.sql
@@ -1,26 +1,31 @@
 Use college;
 
--- TASK 1 - Відобразити рік, назву місяця та кількість студентів вашої групи, що народилися в цьому місяці та році.
-SET @row_number = 0;
-SELECT (@row_number:=@row_number + 1) as "№", YEAR(date_of_birth) as "Рік", MONTHNAME(date_of_birth) as "Місяць"
-FROM student
-WHERE MONTH(date_of_birth) = MONTH(NOW())
-and YEAR(date_of_birth) = YEAR(NOW())
-and id_gr = 2;
+-- TASK 1 - Відобразити прізвища студентів, рік народження яких знаходитися в діапазоні 2000-2002.
+SELECT surname_s as "Прізвище" FROM student
+WHERE date_of_birth between '2000-01-01' and '2002-01-01';
 
--- TASK 2 - Виведіть П.І.Б студентів, що народилися в поточному місяці.
-SELECT surname_s as "Прізвище", sname_ as "Ім'я", slast_name as "по Батькові", date_of_birth as "Дата" 
-FROM student
-WHERE MONTH(date_of_birth) = MONTH(NOW());
+-- TASK 2 - Відобразити кількість студентів МОЄЇ ГРУПИ (2).
+SELECT COUNT(*) as "COUNT" FROM student
+WHERE id_gr = 2;
 
--- TASK 3 - Скільки мається співробітників кожної статі (чоловіків і жінок)?
-SELECT gender_t as "Стать", COUNT(gender_t) as "Кількість" 
-FROM teacher 
-GROUP BY gender_t;
+-- TASK 3 - Відобразити назви предметів, які викладаються на третьому курсі.
+SELECT subjects.subject_name as 'Назва предмету', group_.course as 'Курс'
+FROM subjects, group_, DISTRIBUTION, student, academic_perfomance_
+WHERE group_.id_gr=student.id_gr
+and student.id_st=academic_perfomance_.id_st 
+and academic_perfomance_.id_d=DISTRIBUTION.id_d
+and DISTRIBUTION.id_sub=subjects.id_sub
+and group_.course=3 
+GROUP BY 1;
 
--- TASK 4 - Знайдіть імена 5 наймолодших студентів вашої групи.
-SELECT sname_ as "Ім'я", date_of_birth as "Дата"
-FROM student
-WHERE id_gr = 2
-ORDER BY date_of_birth
-LIMIT 5; 
+-- TASK 4 - Порахувати середній бал для кожного студента.
+SELECT CONCAT(surname_s, ' ', sname_) as "Прізвище, ім'я", avg(mark) as "Середній бал"
+FROM student, academic_perfomance_
+WHERE student.id_st=academic_perfomance_.id_st
+GROUP BY 1;
+
+-- TASK 5 - Відобразити прізвища викладачів комісії Математики.
+SELECT full_name_t as 'Прізвища'
+FROM chair, teacher
+WHERE chair.id_ch=teacher.id_ch
+and cname='Математики'
