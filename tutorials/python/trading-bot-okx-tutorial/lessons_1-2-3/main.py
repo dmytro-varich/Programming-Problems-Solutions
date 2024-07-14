cd1a159c2aaf670962bdcf5dfd4cc74925b41f3d
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-07-14 20:37:43 +0200

    Update main.py

--- tutorials/python/trading-bot-okx-tutorial/lessons_1-2-3/main.py
+++ tutorials/python/trading-bot-okx-tutorial/lessons_1-2-3/main.py
@@ -1,19 +1,46 @@
-import traceback
-from bot.Logger import setup_logger
-logger = setup_logger()
+from okx.PublicData import PublicAPI
+from okx.MarketData import MarketAPI
+from okx.Account import AccountAPI
+from okx.Trade import TradeAPI
 
-from okx.exceptions import OkxAPIException
-from bot.Bot import Bot
-from dotenv import load_dotenv
-load_dotenv() 
+domain = "https://www.okx.com"
+flag = "1"
 
-if __name__ == '__main__':
-    try: 
-        Bot().run()
-    except KeyboardInterrupt as e:
-        logger.debug("Bot was stopped!")
-    except OkxAPIException as e: 
-        logger.debug(str(e))
-    except Exception as e:
-        logger.error("Ошибка: %s", e)
-        logger.error("Traceback: %s", traceback.format_exc())
+
+pub_api = PublicAPI(flag=flag, domain=domain, debug=False)
+instruments = pub_api.get_instruments("SPOT")
+print(instruments.get('data'))
+
+
+market_api = MarketAPI(flag=flag, domain=domain, debug=False)
+candlesticks = market_api.get_candlesticks("BTC-USDT")
+print(candlesticks)
+
+
+from config import API_KEY, SECRET, PASSPHRASE
+acc_api = AccountAPI(API_KEY, SECRET, PASSPHRASE, flag=flag, domain=domain)
+acc_balance = acc_api.get_account_balance()
+print(acc_balance)
+
+SYMBOL="BTC-USDT"
+QTY = 0.00001
+
+trade_api = TradeAPI(API_KEY, SECRET, PASSPHRASE, flag=flag, domain=domain)
+place_order = trade_api.place_order(
+    instId=SYMBOL,
+    tdMode='cash',
+    side='buy', 
+    ordType='market', 
+    sz=QTY, 
+    tgtCcy='base_ccy'
+)
+print(place_order)
+
+orders_history = trade_api.get_orders_history('SPOT')
+current_order = trade_api.get_order(SYMBOL, None)
+print(orders_history)
+
+
+ticker = market_api.get_ticker(SYMBOL)
+print(ticker)
+print(ticker.get('data')[0].get('last'))
