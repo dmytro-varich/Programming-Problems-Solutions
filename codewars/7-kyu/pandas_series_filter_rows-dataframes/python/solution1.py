a6b85e5f5f5d1774bdec3b3fa4e1ff4f1ec9901e
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-07-20 22:13:44 +0200

    Update solution1.py

--- codewars/7-kyu/pandas_series_filter_rows-dataframes/python/solution1.py
+++ codewars/7-kyu/pandas_series_filter_rows-dataframes/python/solution1.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def filter_dataframe(df: pd.DataFrame, col: str, func) -> pd.DataFrame: 
+    if col not in df.columns:
+        raise ValueError(f"Column '{col}' is not present in the DataFrame.")
+        
+    re_func = lambda x: ~df[col].apply(func)
+    filter_ = re_func(df[col])
+    return pd.DataFrame(df[filter_])
