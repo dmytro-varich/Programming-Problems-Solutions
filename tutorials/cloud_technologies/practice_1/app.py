6d7c66baad998c14ee7fa209b3fb102fc7196d2b
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2025-03-10 12:26:09 +0100

    Update app.py

--- tutorials/cloud_technologies/practice_1/app.py
+++ tutorials/cloud_technologies/practice_1/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from datetime import date
+import psycopg2
 
 app = Flask(__name__)
 
@@ -12,9 +13,15 @@ def hello():
     return "<h1 style='color:blue'>Hello There {}!</h1>".format(name)
 
 @app.route("/date")
-def date():
+def calendar():
     today = date.today()
     return "<p>{}</p>".format(today.strftime("%d.%m.%Y"))
+
+@app.route("/connect")
+def connect():
+ connection = psycopg2.connect(database="postgres", user='postgres', 
+                               password='mysecretpassword', host='database')
+ return "<h1 style='color:blue'>Connected!</h1>"
 
 if __name__ == "__main__":
     app.run()
